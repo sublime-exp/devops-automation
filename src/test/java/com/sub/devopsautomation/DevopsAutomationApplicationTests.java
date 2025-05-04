d5967b396c47b8bd2321a2b5578599554668d4bf
sublime-exp <ivailonikolov97@gmail>
2025-05-04 12:26:00 +0300

    Initial

---
+++ src/test/java/com/sub/devopsautomation/DevopsAutomationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sub.devopsautomation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsAutomationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
